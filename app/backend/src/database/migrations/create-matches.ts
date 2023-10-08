385a83a5a6e1f873a4bf75ff87422a33e19c12a7
Lionel Miranda <bibliothecalionel@gmail.com>
2023-10-08 19:33:20 -0400

    feat: ORM migrations

---
+++ app/backend/src/database/migrations/create-matches.ts
@@ -0,0 +1,59 @@
+'use strict';
+import { DataTypes, Model, QueryInterface } from 'sequelize';
+import { IMatch } from '../../Interfaces/IMatch';
+
+export default {
+  up(queryInterface: QueryInterface) {
+    return queryInterface.createTable<Model<IMatch>>('matches', {
+      id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+      },
+      homeTeamId: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        field: 'home_team_id',
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE',
+        references: {
+          model: 'teams',
+          key: 'id',
+        },
+      },
+      homeTeamGoals: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        field: 'home_team_goals',
+      },
+      awayTeamId: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        field: 'away_team_id',
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE',
+        references: {
+          model: 'teams',
+          key: 'id',
+        },
+      },
+      awayTeamGoals: {
+        type: DataTypes.BOOLEAN,
+        allowNull: false,
+        field: 'in_progress',
+        defaultValue: true,
+      },
+      inProgress: {
+        type: DataTypes.BOOLEAN,
+        allowNull: false,
+        field: 'in_progress',
+        defaultValue: true,
+      },
+    })
+  },
+
+  down(queryInterface: QueryInterface) {
+    return queryInterface.dropTable('matches');
+  }
+};
---
+++ app/backend/src/database/models/Teams.ts
@@ -0,0 +1,35 @@
+import {
+  DataTypes,
+  Model,
+  InferAttributes,
+  InferCreationAttributes,
+  CreationOptional,
+} from 'sequelize';
+
+import sequelize from '.';
+
+class Teams extends
+  Model<InferAttributes<Teams>,
+  InferCreationAttributes<Teams>> {
+  declare id: CreationOptional<number>;
+  declare teamName: string;
+}
+
+Teams.init({
+  id: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    autoIncrement: true,
+    primaryKey: true,
+  },
+  teamName: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    field: 'team_name',
+  },
+}, {
+  sequelize,
+  modelName: 'teams',
+  underscored: true,
+  timestamps: false,
+});
